466ea15c0f45216c0f83ea4e0fa32f186e1dedbd
LyndonTomas <201801180@iacademy.edu.ph>
2025-04-07 10:34:04 +0800

    first commit

---
+++ src/webparts/eventRegistrationApp/EventRegistrationAppWebPart.ts
@@ -0,0 +1,205 @@
+import { Version } from '@microsoft/sp-core-library';
+import {
+  type IPropertyPaneConfiguration,
+  PropertyPaneTextField
+} from '@microsoft/sp-property-pane';
+import { BaseClientSideWebPart } from '@microsoft/sp-webpart-base';
+//import type { IReadonlyTheme } from '@microsoft/sp-component-base';
+//import { escape } from '@microsoft/sp-lodash-subset';
+
+import styles from './EventRegistrationAppWebPart.module.scss';
+import * as strings from 'EventRegistrationAppWebPartStrings';
+import { SPHttpClient, SPHttpClientResponse, ISPHttpClientOptions } from '@microsoft/sp-http';
+
+export interface IEventRegistrationAppWebPartProps {
+  description: string;
+}
+
+export default class EventRegistrationAppWebPart extends BaseClientSideWebPart<IEventRegistrationAppWebPartProps> {
+
+  // private _isDarkTheme: boolean = false;
+  // private _environmentMessage: string = '';
+
+  // functions start
+  private CreateItem(): void{
+
+    // get inputs
+    const userName = document.getElementById("txtUserName") as HTMLInputElement;
+    const userNameValue: string = userName.value;
+    const email = document.getElementById("txtEmail") as HTMLInputElement;
+    const emailValue: string = email.value;
+    const batch = document.getElementById("ddlBatch") as HTMLSelectElement;
+    const batchValue: string = batch.value;
+    const levelOfKnowledge = document.getElementById("ddlLevelOfKnowledge") as HTMLSelectElement;
+    const levelOfKnowledgeValue: string = levelOfKnowledge.value;
+    //const siteURL: string  = this.context.pageContext.site.absoluteUrl+"/Lyndon/_api/web/lists/getbytitle('Registration Project')/items";
+    const siteURL : string = `${this.context.pageContext.site.absoluteUrl}/Lyndon/_api/web/lists/getbytitle('${encodeURIComponent('Registration Project')}')/items`;
+
+
+    const itemBody:any = {
+      "Title": userNameValue,
+      "Email": emailValue,
+      "Batch": batchValue,
+      "LevelofKnowledge": levelOfKnowledgeValue
+    }
+
+
+    const spHttpClientOptions: ISPHttpClientOptions = {
+      body: JSON.stringify(itemBody)
+    }
+
+    this.context.spHttpClient.post(siteURL, SPHttpClient.configurations.v1, spHttpClientOptions)
+    .then((response:SPHttpClientResponse) =>{
+      if(response.status ===201){
+        const statusmessage:Element = document.getElementById("divStatus") as HTMLDivElement;
+        statusmessage.innerHTML = "Item Created Successfully";
+      }else{
+        const statusmessage:Element = document.getElementById("divStatus") as HTMLDivElement;
+        statusmessage.innerHTML = "An error has occurred " + response.status+" - "+ response.statusText;
+      }
+    })
+
+  }
+  private  _bindAllEvents(): void {
+    this.domElement.querySelector('#btnCreate')?.addEventListener('click', () =>{
+      this.CreateItem();
+    })
+  }
+  // functions end
+
+
+  public render(): void {
+    this.domElement.innerHTML = `
+    <div class="${styles.welcome}">
+    <table border="4">
+        <tr>
+            <td><b>Enter User ID</b></td>
+            <td><input type="text" id="txtID"></td>
+            <td>
+                <input type="submit" id="btnRead" value="Read Registered User Info">
+            </td>
+        </tr>
+        <tr>
+            <td>User Name</td>
+            <td><input type="text" id="txtUserName"></td>
+        </tr>
+        <tr>
+            <td>Email</td>
+            <td><input type="email" id="txtEmail"></td>
+        </tr>
+        <tr>
+            <td>Enter Batch</td>
+            <td>
+                <select name="Batch" id="ddlBatch">
+                    <option value="Batch 1">Batch 1</option>
+                    <option value="Batch 2">Batch 2</option>
+                    <option value="Batch 3">Batch 3</option>
+                </select>
+            </td>
+        </tr>
+        <tr>
+            <td>Enter Level of Knowledge</td>
+            <td>
+                <select name="LevelOfKnowledge" id="ddlLevelOfKnowledge">
+                    <option value="Beginner">Beginner</option>
+                    <option value="Intermediate">Intermediate</option>
+                    <option value="Expert">Expert</option>
+                </select>
+            </td>
+        </tr>
+        <tr>
+            <td>
+                <input type="submit" value="Create" id="btnCreate">
+                
+                <input type="submit" value="Read" id="btnRead">
+                
+                <input type="submit" value="Update" id="btnUpdate">
+                
+                <input type="submit" value="Delete" id="btnDelete">
+            </td>
+        </tr>
+    </table>
+    <div id="divStatus"></div>
+  </div>`;
+  this._bindAllEvents();
+  }
+
+  // protected onInit(): Promise<void> {
+  //   return this._getEnvironmentMessage().then(message => {
+  //     this._environmentMessage = message;
+  //   });
+  // }
+
+
+
+  // private _getEnvironmentMessage(): Promise<string> {
+  //   if (!!this.context.sdks.microsoftTeams) { // running in Teams, office.com or Outlook
+  //     return this.context.sdks.microsoftTeams.teamsJs.app.getContext()
+  //       .then(context => {
+  //         let environmentMessage: string = '';
+  //         switch (context.app.host.name) {
+  //           case 'Office': // running in Office
+  //             environmentMessage = this.context.isServedFromLocalhost ? strings.AppLocalEnvironmentOffice : strings.AppOfficeEnvironment;
+  //             break;
+  //           case 'Outlook': // running in Outlook
+  //             environmentMessage = this.context.isServedFromLocalhost ? strings.AppLocalEnvironmentOutlook : strings.AppOutlookEnvironment;
+  //             break;
+  //           case 'Teams': // running in Teams
+  //           case 'TeamsModern':
+  //             environmentMessage = this.context.isServedFromLocalhost ? strings.AppLocalEnvironmentTeams : strings.AppTeamsTabEnvironment;
+  //             break;
+  //           default:
+  //             environmentMessage = strings.UnknownEnvironment;
+  //         }
+
+  //         return environmentMessage;
+  //       });
+  //   }
+
+  //   return Promise.resolve(this.context.isServedFromLocalhost ? strings.AppLocalEnvironmentSharePoint : strings.AppSharePointEnvironment);
+  // }
+
+  // protected onThemeChanged(currentTheme: IReadonlyTheme | undefined): void {
+  //   if (!currentTheme) {
+  //     return;
+  //   }
+
+  //   this._isDarkTheme = !!currentTheme.isInverted;
+  //   const {
+  //     semanticColors
+  //   } = currentTheme;
+
+  //   if (semanticColors) {
+  //     this.domElement.style.setProperty('--bodyText', semanticColors.bodyText || null);
+  //     this.domElement.style.setProperty('--link', semanticColors.link || null);
+  //     this.domElement.style.setProperty('--linkHovered', semanticColors.linkHovered || null);
+  //   }
+
+  // }
+
+  protected get dataVersion(): Version {
+    return Version.parse('1.0');
+  }
+
+  protected getPropertyPaneConfiguration(): IPropertyPaneConfiguration {
+    return {
+      pages: [
+        {
+          header: {
+            description: strings.PropertyPaneDescription
+          },
+          groups: [
+            {
+              groupName: strings.BasicGroupName,
+              groupFields: [
+                PropertyPaneTextField('description', {
+                  label: strings.DescriptionFieldLabel
+                })
+              ]
+            }
+          ]
+        }
+      ]
+    };
+  }
+}
---
+++ src/webparts/eventRegistrationApp/loc/mystrings.d.ts
@@ -0,0 +1,19 @@
+declare interface IEventRegistrationAppWebPartStrings {
+  PropertyPaneDescription: string;
+  BasicGroupName: string;
+  DescriptionFieldLabel: string;
+  AppLocalEnvironmentSharePoint: string;
+  AppLocalEnvironmentTeams: string;
+  AppLocalEnvironmentOffice: string;
+  AppLocalEnvironmentOutlook: string;
+  AppSharePointEnvironment: string;
+  AppTeamsTabEnvironment: string;
+  AppOfficeEnvironment: string;
+  AppOutlookEnvironment: string;
+  UnknownEnvironment: string;
+}
+
+declare module 'EventRegistrationAppWebPartStrings' {
+  const strings: IEventRegistrationAppWebPartStrings;
+  export = strings;
+}
